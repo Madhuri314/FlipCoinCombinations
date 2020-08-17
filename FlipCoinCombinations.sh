d05178c53da6b5b9e5eaa6e20a7b3d0a2fefc79a
Madhuri Jagdeo <madhuri.jagdeo21@gmail.com>
2020-08-18 01:59:08 +0530

    Stored the Singlet Combination in a Dictionary and determined the percentage of the Singlet Combination

--- FlipCoinCombinations.sh
+++ FlipCoinCombinations.sh
@@ -1,11 +1,26 @@
 #!/bin/bash -x
 
-ranCheck=$((RANDOM % 2))
+declare -A singletDictionary
 
-if [ $ranCheck -eq 1 ]
-then
-        echo "Heads"
-else
-        echo "Tails"
-fi
+heads=0
+tails=0
 
+for ((counter=0; counter<20; counter++))
+do
+        ranCheck=$((RANDOM % 2))
+        if [ $ranCheck -eq 1 ]
+        then
+                ((heads++))
+        else
+                ((tails++))
+        fi
+done
+
+singletDictionary[Head]=$heads
+singletDictionary[Tail]=$tails
+
+for i in ${!singletDictionary[@]}
+do
+        percentResult=$((${singletDictionary[$i]} * 100 / 20))
+        echo $i" percentage: "$percentResult
+done
